9bbd0ae6b8f18b2c2fa902508799384cb8a353d9
Leonardo <45024471+LEMNXCIX@users.noreply.github.com>
2025-03-27 16:51:37 -0500

    Mejora a la funcion de formatear el nombre

--- src-tauri/src/common/utils.rs
+++ src-tauri/src/common/utils.rs
@@ -3,9 +3,25 @@ pub fn format_name_project(name: &str) -> Result<String, String> {
         let error_message = "Ingrese un valor.".to_string();
         return Err(error_message);
     }
-    //Se formatea el nombre del proyecto
-    let formatted_name = name.replace("_", ": ").replace("-", " ");
 
+    // Divide el prefijo del resto con el primer guion bajo
+    let parts: Vec<&str> = name.splitn(2, "_").collect();
+
+    if parts.len() < 2 {
+        let error_message = "Formato inválido. Asegúrate de incluir al menos un guion bajo.".to_string();
+        return Err(error_message);
+    }
+
+    // Formateo del prefijo
+    let prefix = parts[0].to_uppercase(); // Convierte el prefijo a mayúsculas
+
+    // Formateo del resto del texto
+    let rest = parts[1]
+        .replace("_", ": ")  // Reemplaza guiones bajos por ": "
+        .replace("-", " ");  // Reemplaza guiones medios por espacios
+
+    // Combina el prefijo formateado con el resto del texto
+    let formatted_name = format!("{}: {}", prefix, rest);
     Ok(formatted_name)
 }
 
